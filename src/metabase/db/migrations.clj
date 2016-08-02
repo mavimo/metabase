2cfafcee0132a339f09f7d47eac40a1fa1e2d704
Cam Saül <cammsaul@gmail.com>
2016-08-01 18:12:47 -0700

    Data migrations to create default perms groups :yum: [ci skip]

--- src/metabase/db/migrations.clj
+++ src/metabase/db/migrations.clj
@@ -8,12 +8,16 @@
                              [card :refer [Card]]
                              [dashboard-card :refer [DashboardCard]]
                              [database :refer [Database]]
+                             [database-permissions :refer [DatabasePermissions]]
                              [field :refer [Field]]
                              [interface :refer [defentity]]
+                             [permissions-group :as perm-group]
+                             [permissions-group-membership :refer [PermissionsGroupMembership]]
                              [raw-column :refer [RawColumn]]
                              [raw-table :refer [RawTable]]
                              [table :refer [Table] :as table]
-                             [setting :as setting])
+                             [setting :as setting]
+                             [user :refer [User]])
             [metabase.util :as u]))
 
 ;;; # Migration Helpers
@@ -236,3 +240,29 @@
                                  :last_analyzed (u/new-sql-timestamp))
                                ;; add this column to the set we've processed already
                                (swap! processed-fields conj column-name)))))))))))))))))
+
+
+;; Add users to default permissions groups. This will cause the groups to be created if needed as well.
+(defmigration add-users-to-default-permissions-groups
+  (let [{default-group-id :id} (perm-group/default)
+        {admin-group-id :id}   (perm-group/admin)]
+    (doseq [{user-id :id, superuser? :is_superuser} (db/select [User :id :is_superuser])]
+      (db/insert! PermissionsGroupMembership
+        :user_id  user-id
+        :group_id default-group-id)
+      (when superuser?
+        (db/insert! PermissionsGroupMembership
+          :user_id  user-id
+          :group_id admin-group-id)))))
+
+;; add existing databases to default permissions groups.
+(defmigration add-databases-to-default-permissions-groups
+  (let [{default-group-id :id} (perm-group/default)
+        {admin-group-id :id}   (perm-group/admin)]
+    (doseq [database-id (db/select-ids Database)]
+      (db/insert! DatabasePermissions
+        :database_id database-id
+        :group_id    default-group-id)
+      (db/insert! DatabasePermissions
+        :database_id database-id
+        :group_id    admin-group-id))))
--- src/metabase/models/permissions_group.clj
+++ src/metabase/models/permissions_group.clj
@@ -1,4 +1,21 @@
 (ns metabase.models.permissions-group
-  (:require [metabase.models.interface :as i]))
+  (:require [metabase.db :as db]
+            [metabase.models.interface :as i]))
 
 (i/defentity PermissionsGroup :permissions_group)
+
+(defn default
+  "Fetch the `Default` permissions group, creating it if needed."
+  ^metabase.models.permissions_group.PermissionsGroupInstance []
+  (or (db/select-one PermissionsGroup
+        :name "Default")
+      (db/insert! PermissionsGroup
+        :name "Default")))
+
+(defn admin
+  "Fetch the `Admin` permissions group, creating it if needed."
+  ^metabase.models.permissions_group.PermissionsGroupInstance []
+  (or (db/select-one PermissionsGroup
+        :name "Admin")
+      (db/insert! PermissionsGroup
+        :name "Admin")))
