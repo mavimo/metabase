70cff67c03e6dbae3c9888aa0c9aaf670d86ce44
Cam Saül <cammsaul@gmail.com>
2016-08-01 21:32:47 -0700

    Update WIP permissions queries :lock_with_ink_pen: [ci skip]

--- src/metabase/query_processor/permissions.clj
+++ src/metabase/query_processor/permissions.clj
@@ -2,7 +2,10 @@
   "Logic related to whether a given user has permissions to run/edit a given query."
   (:require [metabase.db :as db]))
 
-(defn- user-has-native-query-write-access? [user-id database-id]
+(defn user-has-native-query-write-access?
+  "Does User with USER-ID have appropriate permissions to *edit* or *delete* a *native* query against database with DATABASE-ID?
+   (Users can always *run* existing native queries.)"
+  [user-id database-id]
   (boolean (seq (db/query {:select    [:dp.native_query_write_access]
                            :from      [[:core_user :u]]
                            :left-join [[:permissions_group_membership :m]  [:= :u.id        :m.user_id]
@@ -13,15 +16,22 @@
                                             [:= :dp.native_query_write_access true]]
                            :limit     1}))))
 
-(defn- x []
-  ((resolve 'db/honeysql->sql) {:select    [:*]
-                                :from      [[(db/entity->table-name 'User) :u]]
-                                :left-join [[:permissions_group_membership :m] [:= :u.id :m.user_id]
-                                            [:permissions_group :g] [:= :m.group_id :g.id]
-                                            [:database_permissions :dp] [:= :dp.group_id :g.id]]
-                                :where     [:and
-                                            [:= :u.id 1]
-                                            [:= :dp.database_id 1]]}))
-
-(defn user-can-run-query-referencing-tables? [user-id database-id table-ids]
-  {:pre [(integer? user-id) (integer? database-id) (set? table-ids) (every? integer? table-ids)]})
+(defn user-can-run-query-referencing-table?
+  "Does User with USER-ID have appropriate permissions to run an MBQL query referencing table with TABLE-ID?"
+  [user-id table-id]
+  {:pre [(integer? user-id) (integer? table-id)]}
+  (boolean (seq (db/query {:select    [true]
+                           :from      [[:permissions_group :g]]
+                           :join      [[:permissions_group_membership :m]  [:= :g.id :m.group_id]
+                                       [:database_permissions         :dp] [:= :g.id :dp.group_id]
+                                       [:metabase_table               :t]  [:= :t.db_id :dp.database_id]]
+                           :left-join [[:schema_permissions           :sp] [:and [:= :g.id :sp.group_id]
+                                                                                 [:= :sp.database_id :t.db_id]
+                                                                                 [:= :sp.schema :t.schema]]
+                                       [:table_permissions            :tp] [:= :g.id :tp.group_id]]
+                           :where     [:and [:= :m.user_id user-id]
+                                            [:= :t.id table-id]
+                                            [:or [:= :dp.unrestricted_schema_access true]
+                                                 [:= :sp.unrestricted_table_access true]
+                                                 [:= :tp.table_id :t.id]]]
+                           :limit     1}))))
